b21c3fdb7f961e694de99665a5ce040c17f6c590
Guillermo Julián <gjulianm@outlook.com>
2014-10-21 10:33:16 +0200

    Añadidos géneros

--- P2/js/controllers.js
+++ P2/js/controllers.js
@@ -1,21 +1,39 @@
 var mainApp = angular.module('mainApp', []);
 
+function unique(arr) {
+    var u = {}, a = [];
+    for(var i = 0, l = arr.length; i < l; ++i){
+        if(!u.hasOwnProperty(arr[i])) {
+            a.push(arr[i]);
+            u[arr[i]] = 1;
+        }
+    }
+    return a;
+}
+
 mainApp.controller('movieListController', function($scope){
 	$scope.movies = [
 		{ 
 			'image' : 'img/movie.jpg',
 			'title' : 'Título',
-			'description': "Patatas fritsa"
+			'description': "Patatas fritsa",
+			'genre': 'Terror'
 		},
 		{ 
 			'image' : 'img/movie.jpg',
 			'title' : 'Título123',
-			'description': "aaaaa fritsa"
+			'description': "aaaaa fritsa",
+			'genre': 'Patat'
 		},
 		{ 
 			'image' : 'img/movie.jpg',
 			'title' : 'Título 2 ',
-			'description': "Patatas asdasd"
+			'description': "Patatas asdasd",
+			'genre': "Comedia"
 		}
 	];
+
+	$scope.genres = unique($scope.movies.map(function(movie) {
+		return movie['genre'];
+	}))
 });
