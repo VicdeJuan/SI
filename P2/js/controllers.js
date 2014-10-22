cdacd966210d8df164c33b6ffe483472ee0231ee
Guillermo Julián <gjulianm@outlook.com>
2014-10-22 23:08:55 +0200

    Estructurado algo mejor el carrito, ya es inteligente y cuenta bien y todo.

--- P2/js/controllers.js
+++ P2/js/controllers.js
@@ -1,31 +1,52 @@
 var mainApp = angular.module('mainApp', ['ngAnimate']);
 
 function unique(arr) {
-    var u = {}, a = [];
-    for(var i = 0, l = arr.length; i < l; ++i){
-        if(!u.hasOwnProperty(arr[i])) {
-            a.push(arr[i]);
-            u[arr[i]] = 1;
-        }
-    }
-    return a;
+	var u = {}, a = [];
+	for(var i = 0, l = arr.length; i < l; ++i){
+		if(!u.hasOwnProperty(arr[i])) {
+			a.push(arr[i]);
+			u[arr[i]] = 1;
+		}
+	}
+	return a;
 }
 
-var cart = [
-{ 
-	'image' : 'img/movie.jpg',
-	'title' : 'Título',
-	'description': "Patatas fritsa",
-	'genre': 'Terror',
-	'price': 4
-},{ 
-	'image' : 'img/movie.jpg',
-	'title' : 'Título',
-	'description': "Patatas fritsa",
-	'genre': 'Terror',
-	'price': 4
-}		
-];
+var cart = {
+	items: [],
+
+	find: function(title) {
+		for(var i = this.items.length; i--;) {
+			if(this.items[i]['title'] === title) {
+				return this.items[i];
+			}
+		}
+		return null;
+	},
+
+	add: function(toAdd) {
+		var existing = this.find(toAdd['title']);
+
+		if(existing != null)
+			existing['quantity'] += 1;
+		else
+		{
+			toAdd['quantity'] = 1;
+			this.items.push(toAdd);
+		}
+	},
+
+	remove: function(toRemove) {
+		var existing = this.find(toRemove['title']);
+
+		if(existing == null)
+			return;
+
+		existing['quantity'] -= 1;
+
+		if(existing['quantity'] <= 0)
+			remove(this.items, existing);
+	}
+}
 
 function remove(arr, item) {
 	for(var i = arr.length; i--;) {
@@ -36,36 +57,36 @@ function remove(arr, item) {
 }
 
 mainApp.controller('headerController', function($scope) {
-	$scope.cartItems = cart;
+	$scope.cartItems = cart.items;
 
 	$scope.removeFromCart = function(item) {
-		remove(cart, item);
+		cart.remove(item);
 	};
 });
 
 mainApp.controller('movieListController', function($scope){
 	$scope.movies = [
-		{ 
-			'image' : 'img/movie.jpg',
-			'title' : 'Título',
-			'description': "Patatas fritsa",
-			'genre': 'Terror',
-			'price': 4
-		},
-		{ 
-			'image' : 'img/movie.jpg',
-			'title' : 'Título123',
-			'description': "aaaaa fritsa",
-			'genre': 'Patat',
-			'price': 2
-		},
-		{ 
-			'image' : 'img/movie.jpg',
-			'title' : 'Título 2 ',
-			'description': "Patatas asdasd",
-			'genre': "Comedia",
-			'price': 1
-		}
+	{ 
+		'image' : 'img/movie.jpg',
+		'title' : 'Título',
+		'description': "Patatas fritsa",
+		'genre': 'Terror',
+		'price': 4
+	},
+	{ 
+		'image' : 'img/movie.jpg',
+		'title' : 'Título123',
+		'description': "aaaaa fritsa",
+		'genre': 'Patat',
+		'price': 2
+	},
+	{ 
+		'image' : 'img/movie.jpg',
+		'title' : 'Título 2 ',
+		'description': "Patatas asdasd",
+		'genre': "Comedia",
+		'price': 1
+	}
 	];
 
 	$scope.genres = unique($scope.movies.map(function(movie) {
@@ -73,7 +94,6 @@ mainApp.controller('movieListController', function($scope){
 	}));
 
 	$scope.addToCart = function(movie) {
-		movie['quantity'] = 1;
-		cart.push(movie);
+		cart.add(movie);
 	}
 });
