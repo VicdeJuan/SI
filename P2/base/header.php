667e09e6844c97951291ba231905c5fe89bfa671
Guillermo Julián <gjulianm@outlook.com>
2014-10-27 14:56:06 +0100

    Arreglado fallo del controlador en el carrito

--- P2/base/header.php
+++ P2/base/header.php
@@ -1,76 +1,79 @@
-<html lang="en" xmlns:ng="http://angularjs.org" ng-app="mainApp">
-<head>
-	<link rel="stylesheet" type="text/css" href="style/main.css" />
-	<meta charset="utf-8" />
-	<title>Ola k ase</title>
-	<script src="lib/angular.min.js" type="text/javascript"></script>
-	<script src="lib/angular-animate.min.js" type="text/javascript"></script>
-	<script src="js/controllers.js" type="text/javascript"></script>
+<?php require 'html_head.php'; ?>
 
-	<body>
-		<header ng-controller="headerController" ng-init="showCart = false">
-			<div class="header-logo">
-				<p>Olakase</p>
-			</div>
-			<div class="header-options">
-				<?php
-				session_start();
-				if($_SESSION['name'] == "" ){
-					$link = "";
-					$text = "Login";
-				}else{
-					$text = $_SESSION['name'];
-					$link = "/pages/error.html";
-				} ?>
-				<ul ng-controller="loginSubmitController" ng-init="showLogin = false;loginTitle ='<?php echo $text; ?>'" >
-					<li>
-						<a ng-click="showLogin = !showLogin;" href="<?php echo $link; ?>">
-							{{ loginTitle }}
-						</a>
-					</li>
+<?php
+	session_start();
 
-					<div class="login-div" ng-show="showLogin" ng-click="showLogin = false" >
-						<!-- TODO:  petición http y procesar código de error -->
-						<form ng-click="$event.stopPropagation();" ng-submit="loginSubmit();" name="login-form"  class="login-form" ng-show="showLogin" >
-							<table>
-								<tr>
-									<td>Email:</td>
-									<td> <input type="email" name="email" ng-model="email" ></td>
-								</tr>
-								<tr>
-									<td>Contraseña:</td>
-									<td> <input type="password" name="password" ng-model="password" autocomplete="off" required pattern="[a-zA-Z0-9]+" id="passwordfield"></td>
-								</tr>	
-								<tr>
-									<td colspan="2">
-										<div id="messages" class="login-err-msg" ng-show="errLogin" > El email y la contraseña no se encuentran en la base de datos. </div>
-									</td>
-								</tr>
-							</table>
-							<p>
-								<a href="register.html" id="NewRegister">¿No tienes cuenta todavía?</a>		
-								<input type="submit" value="login" name="login" id="login-button">
-							</p>			
-						</form>
-					</div>
+	if($_SESSION['name'] == "" )
+	{
+		$link = "";
+		$text = "Login";
+	}
+	else
+	{
+		$text = $_SESSION['name'];
+		$link = "/pages/error.html";
+	} 
+?>
 
-					<li>
-						<a href="" ng-click="showCart = !showCart">Carrito ({{cartItems.length}})</a>
-					</li>
-					<li><a href="/php/exit.php">Salir</a></li>
-				</ul>
-			</div>
-			<div class="cart-container animate-show" ng-show="showCart">
-				<div class="cart">
-					<div class="cartItem" ng-repeat="item in cartItems">
-						<a href="javascript:void()" ng-click="removeFromCart(item)" class="deleteButton"><img ng-src="img/close.svg"></a>
-						<img class="cartItemImage" src="{{item.image}}" />
-						<div class="cartItemPrice"><p>{{item.quantity}} x {{item.price}}€</p></div>
-						<div class="cartItemTitle"><p>{{item.title}}</p></div>
-					</div>
+<body>
+<header ng-controller="headerController" ng-init="showCart = false">
+	<div class="header-logo">
+		<p>Olakase</p>
+	</div>
+
+	<div class="header-options">
+		<ul>
+			<span ng-controller="loginSubmitController" ng-init="showLogin = false;loginTitle ='<?php echo $text; ?>'" >
+				<li>
+					<a ng-click="showLogin = !showLogin;" href="<?php echo $link; ?>">
+						{{ loginTitle }}
+					</a>
+				</li>
+
+				<div class="login-div" ng-show="showLogin" ng-click="showLogin = false" >
+					<form ng-click="$event.stopPropagation();" ng-submit="loginSubmit();" name="login-form"  class="login-form" ng-show="showLogin" >
+						<table>
+							<tr>
+								<td>Email:</td>
+								<td> <input type="email" name="email" ng-model="email" ></td>
+							</tr>
+							<tr>
+								<td>Contraseña:</td>
+								<td> <input type="password" name="password" ng-model="password" autocomplete="off" required pattern="[a-zA-Z0-9]+" id="passwordfield"></td>
+							</tr>	
+							<tr>
+								<td colspan="2">
+									<div id="messages" class="login-err-msg" ng-show="errLogin" > El email y la contraseña no se encuentran en la base de datos. </div>
+								</td>
+							</tr>
+						</table>
+						<p>
+							<a href="register.html" id="NewRegister">¿No tienes cuenta todavía?</a>		
+							<input type="submit" value="login" name="login" id="login-button">
+						</p>			
+					</form>
 				</div>
-				<div class="cartBuy">
-					<div class="button">¡Comprar!</div>
-				</div>
+			</span>
+
+			<li>
+				<a href="" ng-click="showCart = !showCart">Carrito ({{cartItems.length}})</a>
+			</li>
+
+			<li><a href="/php/exit.php">Salir</a></li>
+		</ul>
+	</div>
+
+	<div class="cart-container animate-show" ng-show="showCart">
+		<div class="cart">
+			<div class="cartItem" ng-repeat="item in cartItems">
+				<a href="javascript:void()" ng-click="removeFromCart(item)" class="deleteButton"><img ng-src="img/close.svg"></a>
+				<img class="cartItemImage" src="{{item.image}}" />
+				<div class="cartItemPrice"><p>{{item.quantity}} x {{item.price}}€</p></div>
+				<div class="cartItemTitle"><p>{{item.title}}</p></div>
 			</div>
-		</header>
+		</div>
+		<div class="cartBuy">
+			<div class="button">¡Comprar!</div>
+		</div>
+	</div>
+</header>
---
+++ P2/base/html_head.php
@@ -0,0 +1,12 @@
+<html lang="en" xmlns:ng="http://angularjs.org" ng-app="mainApp">
+<head>
+	<link rel="stylesheet" type="text/css" href="style/main.css" />
+	<meta charset="utf-8" />
+	<title>Ola k ase</title>
+	<script src="lib/angular.min.js" type="text/javascript"> 
+	</script>
+	<script src="lib/angular-animate.min.js" type="text/javascript">
+	</script>
+	<script src="js/controllers.js" type="text/javascript">
+	</script>
+</head>
