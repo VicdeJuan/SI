4ce29d444878873c632d024b2ccaa4d998ef0ea7
Guillermo Julián <gjulianm@outlook.com>
2014-10-27 14:36:28 +0100

    Arreglado lío del merge

--- P2/index.php
+++ P2/index.php
@@ -29,34 +29,30 @@
 					</li>
 
 					<div class="login-div" ng-show="showLogin" ng-click="showLogin = false" >
-				 	<!-- TODO:  petición http y procesar código de error -->
-				 		<form ng-click="$event.stopPropagation();" ng-submit="loginSubmit();" name="login-form"  class="login-form" ng-show="showLogin" >
-				 			<table>
+						<!-- TODO:  petición http y procesar código de error -->
+						<form ng-click="$event.stopPropagation();" ng-submit="loginSubmit();" name="login-form"  class="login-form" ng-show="showLogin" >
+							<table>
 								<tr>
-									<td>
-											<td>Email:</td>
-											<td> <input type="email" name="email" ng-model="email" ></td>
-									</td>
+									<td>Email:</td>
+									<td> <input type="email" name="email" ng-model="email" ></td>
 								</tr>
 								<tr>
-									<td>
-										<td>Contraseña:</td>
-										<td> <input type="password" name="password" ng-model="password" autocomplete="off" required pattern="[a-zA-Z0-9]+" id="passwordfield"></td>
+									<td>Contraseña:</td>
+									<td> <input type="password" name="password" ng-model="password" autocomplete="off" required pattern="[a-zA-Z0-9]+" id="passwordfield"></td>
+								</tr>	
+								<tr>
+									<td colspan="2">
+										<div id="messages" class="login-err-msg" ng-show="errLogin" > El email y la contraseña no se encuentran en la base de datos. </div>
 									</td>
-
-								</td>	
-								<td>
-									<div id="messages" class="login-err-msg" ng-show="errLogin" > El email y la contraseña no se encuentran en la base de datos. </div>
-								</td>
 								</tr>
 							</table>
-				 			<p>
-					  			<a href="register.html" id="NewRegister">¿No tienes cuenta todavía?</a>		
-					  			<input type="submit" value="login" name="login" id="login-button">
+							<p>
+								<a href="register.html" id="NewRegister">¿No tienes cuenta todavía?</a>		
+								<input type="submit" value="login" name="login" id="login-button">
 							</p>			
-				 		</form>
-				 	</div>
-					
+						</form>
+					</div>
+
 					<li>
 						<a href="" ng-click="showCart = !showCart">Carrito ({{cartItems.length}})</a>
 					</li>
@@ -79,47 +75,145 @@
 		</header>
 
 		<!-- header.html end -->
- <div class="body-container" id="body-container" ng-controller="movieListController">
- 	<aside class="menu">
- 		<ul>
- 			<li class="filter"><a href="index.html" class="filter-title">Inicio</a></li>
- 			<li class="filter">
- 				<span class="filter-title">Género</span>
- 				<ul class="filter-items">
- 					<li ng-repeat="genre in genres">
- 						<a href="pages/error.html" class="filter-genre">{{genre}}</a>
- 					</li>
- 				</ul>
- 			</li>
- 			<li class="filter"><span class="filter-title">Año</span></li>
- 			<li class="filter"><span class="filter-title">Etc</span></li>
- 		</ul>
- 	</aside>
+		<div class="body-container" ng-controller="movieListController">
+			<aside class="menu">
+				<ul>
+					<li class="filter">
+						<span class="filter-title">Título</span>
+						<p class="filter-items">
+							<input type="text" ng-model="searchTitle" />
+						</p>
+					<li class="filter">
+						<span class="filter-title">Género</span>
+						<ul class="filter-items">
+							<li ng-repeat="genre in genres">
+								<input 
+								type="radio" 
+								value="{{genre}}" 
+								class="filter-genre" 
+								ng-model="$parent.searchGenre" 
+								name="searchGenre">
+									{{genre}}
+								</input>
+							</li>
+							<li>
+								<input 
+								type="radio" 
+								value="" 
+								class="filter-genre" 
+								ng-model="searchGenre" 
+								name="searchGenre">
+									Todos
+								</input>
+							</li>
+						</ul>
+					</li>
 
- 	<div class="scroller">
- 		<div class="inner-scroll">
- 			<div class="main-container" 
- 				ng-controller="movieListController">
- 				<div class="movies" ng-repeat="movie in movies">
- 					<img ng-src="{{movie.image}}" class="movie_img">
- 					<p class="movie_title">{{movie.title}}</p>
- 					<p class="movie_description">
- 						{{movie.description}}
- 					</p>
- 					<a ng-click="addToCart(movie)" href="">Añadir al carrito</a>
- 				</div>
- 			</div>
- 		</div>
- 	</div>
- </div>
+					<li class="filter">
+						<span class="filter-title">Año</span>
+						<ul class="filter-items">
+							<li ng-repeat="year in years">
+								<input 
+								type="radio" 
+								value="{{year.bounds}}" 
+								class="filter-genre" 
+								ng-model="$parent.yearRange" 
+								name="year">
+								{{year.name}}
+							</input>
+							</li>
+							<li>
+								<input ng-init="customYearRange = {}"
+								type="radio" 
+								value="custom"
+								class="filter-genre" 
+								ng-model="yearRange" 
+								name="yearRange">
+									Personalizado: 
+									<input type="number" ng-model="customYearRange.min" /> 
+									hasta 
+									<input type="number" ng-model="customYearRange.max" />.
+								</input>
+							</li>
+							<li>
+								<input 
+								type="radio" 
+								value='{"min":0,"max":5000}'
+								class="filter-genre" 
+								ng-model="yearRange" 
+								name="yearRange">
+								Todos
+								</input>
+							</li>
+						</ul>
+					</li>
 
- 	
- 	<!-- footer.html begin -->
-	<footer>
-		<p class="footer-text">
-			Olakase - Víctor de Juan Sanz - Guillermo Julián Moreno
-		</p>
-	</footer>
+					<li class="filter">
+						<span class="filter-title">Precio</span>
+						<ul class="filter-items">
+							<li ng-repeat="price in prices">
+								<input 
+								type="radio" 
+								value="{{price.bounds}}" 
+								class="filter-genre" 
+								ng-model="$parent.priceRange" 
+								name="price">
+								{{price.name}}
+							</input>
+							</li>
+							<li>
+								<input ng-init="customPriceRange = {}"
+								type="radio" 
+								value="custom"
+								class="filter-genre" 
+								ng-model="priceRange" 
+								name="priceRange">
+								Personalizado: 
+								<input type="number" ng-model="customPriceRange.min" /> hasta <input type="number" ng-model="customPriceRange.max" />.
+								</input>
+							</li>
+							<li>
+								<input 
+								type="radio" 
+								value='{"min":0,"max":5000}'
+								class="filter-genre" 
+								ng-model="priceRange" 
+								name="priceRange">
+								Todos
+								</input>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</aside>
 
-</body>
+			<div class="scroller">
+				<div class="main-container">
+					<p class="pagination-control"><a href="" ng-click="prevPage()">&lt;</a> <a href="" ng-click="nextPage()">&gt;</a> | {{startIndex}} - {{startIndex + filtered.length}} (
+						<select ng-model="pageLength" ng-options="length for length in availableLengths">
+						</select> resultados por página)
+					</p>
+
+					<div class="movies" ng-repeat="movie in movies | movieFilter:search | slice:startIndex | limitTo:pageLength as filtered">
+						<img ng-src="{{movie.image}}" class="movie_img">
+						<p class="movie_title">{{movie.title}}</p>
+						<p class="movie_description">
+							{{movie.price}} €: {{movie.description}}
+						</p>
+						<a ng-click="addToCart(movie)" href="">Añadir al carrito</a>
+					</div>
+
+					<p class="pagination-control"></p>
+				</div>
+			</div>
+		</div>
+
+		<!-- footer.html begin -->
+		<footer>
+			<p class="footer-text">
+				Olakase - Víctor de Juan Sanz - Guillermo Julián Moreno
+			</p>
+		</footer>
+
+	</body>
 </html>
