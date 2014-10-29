ae00c2e93be0bc758853843eebd2dded86295c20
Vicdejuan <dejuan.victor@gmail.com>
2014-10-29 14:34:43 +0100

    Funciones del php completadas. Falta la api en si

--- P2/api/history.php
+++ P2/api/history.php
@@ -9,11 +9,28 @@
 	echo "olakase";
 	require_once '../php/history.php';
 	echo "olakase";
+
+	/* Get */
 	$result = getHistory("../users/","history.xml");
 	if ($result == null)
 		echo "SII";
 	else
-		print(count($result));
+		print(count($result)."\n");
+
+	/* Create */
+	print(createHistory("../users/","asdf"));
+
+
+	/* Add */
+	$movies_id = file_get_contents('php://input');
+
+	$ids = array(array('id' => 0,'cuantity' => 7),array('id' => 7,'cuantity' => 3));
+
+	print("<br><br><br>Aqui es donde mirar: <br>");
+
+
+	print_r(addHistory("../users/","asdf",$ids));
+
 
 	?>
 
--- P2/php/history.php
+++ P2/php/history.php
@@ -1,40 +1,63 @@
 <?php
 
+	function getMoviesFromId($array_ids){
+			$movies = simplexml_load_file('../data/movies.xml');
+			$array = array();
+
+			for ($i=0; $i < count($array_ids); $i++) { 
+				$array[$i] = $movies->movie[$movies_id[$i]];
+			}
+			return $array;
+	}
+
+	function readMoviesHistoryId($file){
+			$movies_id = simplexml_load_file($file)->xpath('movie/id');			
+			for ($i=0; $i < count($movies_id); $i++) { 
+				$movies_id[$i] = (int) $movies_id[$i];
+			}
+			return $movies_id;		
+	}
+
 	function getHistory($dir,$filename){
 
 		$file = fopen($dir.$filename, "r");
 
 		if ($file == null) {
-
-			return 404;
-		}else{
-			$movies_id = simplexml_load_file($dir.$filename)->xpath('movie/id');
 			
-			for ($i=0; $i < count($movies_id); $i++) { 
-				$movies_id[$i] = (int) $movies_id[$i];
-			}
+			return 404;
+		
+		}else{
 
-			$movies = simplexml_load_file('../data/movies.xml');
-			$array = array();
-
-			for ($i=0; $i < count($movies_id); $i++) { 
-				$array[$i] = $movies->movie[$movies_id[$i]];
-			}
-
+			$movies_id = readMoviesHistoryId($dir.$filename);
 			fclose($file);
-			return $array;
+
+			return getMoviesFromId($movies_id);
 		}
 	}
 
-	function addHistory($dir,$filename){
+
+	function addHistory($dir,$filename,$movies_id){
 		$file = fopen($dir.$filename, "a");
 
 		if ($file == null) {
+			fclose($file);
 			return 404;
 		}else{
-			/**
-			 * TODO: code here
-			 */
+			
+			$current = simplexml_load_file("../users/history.xml");			
+
+			foreach ($movies_id as $pair) {
+				$child = $current->addChild('movie');
+				$child->addChild('id',$pair['id']);
+				$child->addChild('cuantity',$pair['cuantity']);
+			}
+
+
+			$current->asXML($dir."updated.xml");
+
+
+			fclose($file);
+			return $current;
 		}
 
 	}
@@ -43,11 +66,12 @@
 		$file = fopen($dir.$filename, "w");
 
 		if ($file == null) {
+			fclose($file);
 			return 404;
 		}else{
-			/**
-			 * TODO: code here
-			 */
+			fwrite($file,"<?xml version=".'"'."1.0".'"'." encoding=".'"'."UTF-8".'"'."?>\n<!DOCTYPE note SYSTEM ".'"'."/data/history.dtd".'"'.">\n<catalog>\n</catalog>\n");
+			fclose($file);
+			return 200;
 		}
 
 	}
