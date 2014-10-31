327efe242d6de20bb87f318e2b177f608526a3b9
Guillermo Julián <gjulianm@outlook.com>
2014-10-31 23:35:37 +0100

    Salida HTML decente.

--- P2/history.php
+++ P2/history.php
@@ -1,13 +1,57 @@
-<?php
-	require_once 'base/header.php';
-	require_once 'php/history.php';
-
-	session_start();
+<?php require_once 'base/header.php'; ?>
 
 
-	$movies = getHistory("users/".$_SESSION['email']);
+<div class="body-container">
+	<aside class="menu">
+		
+	</aside>
 
-	var_dump($movies);
+	<div class="scroller">
+		<div class="main-container">
+			<?php
+				require_once 'php/history.php';
 
-	require_once 'base/footer.php';
-?>
+				$purchases = getHistory("users/".$_SESSION['email']);
+
+				foreach ($purchases as $purchase) 
+				{
+					$price = 0;
+					$movieCount = 0;
+
+					foreach ($purchase['movies'] as $movie) 
+					{
+						$price += $movie['quantity'] * $movie['price'];
+						$movieCount += $movie['quantity'];
+					} 
+			?>
+			<div class="purchase">
+				<div class="purchase-info">
+					<span class="purchase-count"><?php echo $movieCount; ?> películas</span>
+					<span class="purchase-price"><?php echo $price; ?> €</span>
+				</div>
+
+				<div class="purchase-items">
+					<?php foreach ($purchase['movies'] as $movie) { ?>
+					<div class="purchase-item">
+						<img src="<?php echo $movie['image']; ?>" class="purchase-img" />
+						<div class="purchase-title"><?php echo $movie['title']; ?></div>
+
+						<div class="purchase-item-price">
+							<?php echo $movie['quantity']; ?> × <?php echo $movie['price']; ?> €
+						</div>
+					</div>
+					<?php } ?>
+				</div>
+
+				<div class="purchase-date"><?php 
+					$date = DateTime::createFromFormat(DATE_ATOM, $purchase['date']);
+					echo $date->format('d F Y, H:i');
+				?></div>
+			</div>	
+			<?php } ?>
+		</div>
+	</div>
+</div>
+
+
+<?php require_once 'base/footer.php'; ?>
