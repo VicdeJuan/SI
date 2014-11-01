41cc143af0eb8e7c99679ce341a5b23b9d478ba8
Vicdejuan <dejuan.victor@gmail.com>
2014-11-01 18:57:01 +0100

    Arreglado credto por crédito

--- P2/register.php
+++ P2/register.php
@@ -11,7 +11,7 @@
 			> {{ strengthMsg }} </div><br>
 
 			<label class="login-label"> E-mail:</label> <input class="login-input" type="email" name="email" autocomplete="off" required title="Introduzca una dirección de correo válida">
-			<label class="login-label"> Tarjeta de crédto:</label> <input class="login-input" type="text" name="creditCard" required pattern="\d{16}" title="Introduzca un número de tarjeta de crédito válido."><br>
+			<label class="login-label"> Tarjeta de crédito:</label> <input class="login-input" type="text" name="creditCard" required pattern="\d{16}" title="Introduzca un número de tarjeta de crédito válido."><br>
 			
 			<label class="login-label"> Fecha de caducidad (mm/yyyy):</label> <input class="login-input" type="text" name="expireDate" required pattern="\d{1,2}/\d{4}" title="Introduzca el mes y el año (completo) de caducidad de su tarjeta">
 			<label class="login-label"> Codigo de seguridad:</label> <input class="login-input" type="text" name="CSV" required pattern="\d\d\d">
