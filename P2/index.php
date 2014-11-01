106eb5ff73e5953af36ee0c18c3c4f31a938f680
Vicdejuan <dejuan.victor@gmail.com>
2014-11-01 18:09:49 +0100

    Añadido data a algunos ng. A ver si reduce los errores

--- P2/index.php
+++ P2/index.php
@@ -1,6 +1,6 @@
 <?php require $_SERVER['DOCUMENT_ROOT'].'/base/header.php'; ?>
 
-<div class="body-container" ng-controller="movieListController">
+<div class="body-container" data-ng-controller="movieListController">
 	<aside class="menu">
 		<ul class="menu-items"> 
 			<filter title="Título" value="search.title" name"titleFitler" fallback="emptyStrObject" allow-custom="string"></filter>			
@@ -13,24 +13,24 @@
 	<div class="scroller">
 		<div class="scroller-top">
 			<div class="pagination-control">
-				<a href="" class="page-control" ng-class="prevDisabled" ng-click="prevPage()">‹</a> 
+				<a href="" class="page-control" data-ng-class="prevDisabled" data-ng-click="prevPage()">‹</a> 
 				<span>{{startIndex}} - {{startIndex + filtered.length}}</span>
-				<a href="" class="page-control" ng-class="nextDisabled" ng-click="nextPage()">›</a>
+				<a href="" class="page-control" data-ng-class="nextDisabled" data-ng-click="nextPage()">›</a>
 			</div>
 			<div class="pagination-config">
-				<select ng-model="pageLength" ng-options="length for length in availableLengths">
+				<select data-ng-model="pageLength" data-ng-options="length for length in availableLengths">
 				  </select>	resultados por página
 			</div>
 		</div>
-		<div class="main-container" ng-class="movieHoverClass">
-			<div class="movie" ng-repeat="movie in movies | movieFilter:search | slice:startIndex | limitTo:pageLength as filtered">
+		<div class="main-container" data-ng-class="movieHoverClass">
+			<div class="movie" data-ng-repeat="movie in movies | movieFilter:search | slice:startIndex | limitTo:pageLength as filtered">
 				<div class="movie-cover">
-					<img alt="Imagen de la película" ng-src="{{movie.image}}" class="movie-img">
+					<img alt="Imagen de la película" data-ng-src="{{movie.image}}" class="movie-img">
 					<p class="movie-title">{{movie.title}}</p>
 					<div class="movie-action">
 						<p class="movie-price">{{movie.price}} €</p>
 						<p class="movie-buy">
-							<a ng-click="addToCart(movie)" href="">
+							<a data-ng-click="addToCart(movie)" href="">
 								<img  alt="Añadir al carrito" src="/img/cart.svg" class="movie-cart" />
 							</a>
 						</p>
