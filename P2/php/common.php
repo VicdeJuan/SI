93602aa177608eaf5e2bf4c8f93097b905e1d2d5
Guillermo Julián <gjulianm@outlook.com>
2014-11-02 14:50:19 +0100

    Se ve que context_prefix no incluye la barra

--- P2/php/common.php
+++ P2/php/common.php
@@ -1,6 +1,10 @@
 <?php
 
+
 $applicationBaseDir = $_SERVER['CONTEXT_PREFIX'];
+
+if(strlen($applicationBaseDir) == 0 || substr($applicationBaseDir, -1) !== "/")
+	$applicationBaseDir .= '/';
 
 function asAbsolutePath($path) 
 {
