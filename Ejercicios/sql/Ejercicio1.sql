d7366d679db1635d9fc85e6bc5bc38bf30410327
Vicdejuan <dejuan.victor@gmail.com>
2014-11-12 19:11:44 +0100

    Ejercicio 1 a medias, casi casi

---
+++ Ejercicios/sql/Ejercicio1.sql
@@ -0,0 +1,40 @@
+/* Creación de la vista: */
+create or replace view top10protas as select actor_id,sum(ord) as num_protagonista 
+	from reparto,actor,pelicula
+	where pelicula.id = reparto.pelicula_id AND 
+	 	reparto.actor_id = actor.id AND
+	 	 reparto.ord = 1 AND
+	 	 pelicula.agno > 1980 
+	group by actor_id
+	order by num_protagonista
+	desc limit 10;
+
+
+/* Creación de la tabla auxiliar */
+drop table TOP10;
+CREATE TABLE  TOP10 as select actor_id,sum(ord) as num_protagonista 
+	from reparto,actor,pelicula
+	where pelicula.id = reparto.pelicula_id AND 
+	 	reparto.actor_id = actor.id AND
+	 	 reparto.ord = 1 AND
+	 	 pelicula.agno > 1980 
+	group by actor_id
+	order by num_protagonista
+	desc limit 10;
+
+/* Trigger. */
+
+
+
+DROP FUNCTION modificacion2() cascade;
+CREATE FUNCTION modificacion2() RETURNS TRIGGER AS '
+DECLARE 
+  	iterator record;
+BEGIN
+	UPDATE TOP10 SET num_protagonista = num_protagonista + 1 where actor_id = NEW.actor_id;
+RETURN NEW; END;'LANGUAGE 'plpgsql';
+
+
+DROP TRIGGER t_modificacion2 on reparto;
+CREATE TRIGGER t_modificacion2 AFTER INSERT ON reparto
+FOR EACH ROW EXECUTE PROCEDURE modificacion2();
