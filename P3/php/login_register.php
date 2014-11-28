e1d4e4370a12327eff4ab02dc126d908f84ec845
Guillermo Julián <gjulianm@outlook.com>
2014-11-28 12:20:05 +0100

    Medio arreglado login_register

--- P3/php/login_register.php
+++ P3/php/login_register.php
@@ -1,46 +1,55 @@
  <?php
-/*  If we come from a registration  */
-
-require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/php/common.php';
-require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/php/history.php';
-require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/php/sql.php';
+ require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/php/common.php';
+ require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/php/history.php';
+ require_once $_SERVER['CONTEXT_DOCUMENT_ROOT'].'/php/sql.php';
 
 /*
-Campos de customers: 
-customerid , firstname , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" 
+Campos de customers:
+customerid , firstname , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname" , "firstname"
  */
 
 
-$dbh =  DBConnect_PDO();
+$dbh = DBConnect_PDO();
 
+$sql_userSelect = <<<SQL
+SELECT email, username, password
+	FROM customers
+	WHERE email = :email AND password = :password;
+SQL;
 
-$stmt = $dbh->prepare( "SELECT email,username,password FROM customers WHERE email = :email AND password = :password" );
-$stmt_create = $dbh->prepare( "INSERT INTO customers (
-    firstname ,lastname ,address1 ,address2 ,city ,state ,ip ,country,region,email ,phone ,
-    creditcardtype ,creditcard ,creditcardexpiration ,username ,password ,age ,income ,gender)
-  values 
-    (:username ,'lastname' ,'address1' ,'address2' ,'city' ,'state' ,28030 ,'country' ,'region',
-     :email ,'phone' ,'cctype' ,
-     :creditCard,
-     :expireCard ,
-     :username ,
-     :password ,21 ,5 ,'M'); 
-  " );
-$stmt_getid = $dbh->prepare( "SELECT customerid as id, username as name  FROM customers WHERE email = :email" );
+$sql_userCreate = <<<SQL
+INSERT INTO customers (
+	firstname, lastname, address1, address2, city, state, zip,
+	country, region, email, phone, creditcardtype, creditcard,
+	creditcardexpiration, username, password, age, income, gender)
+VALUES (
+	:username, 'lastname', 'address1', 'address2', 'city', 'state', 28030,
+	'country', 'region', :email, 'phone', 'VISA', :creditCard,
+ 	:expireCard, :username , :password, 21, 0, 'M');
+SQL;
 
+$sql_getId = <<<SQL
+SELECT customerid AS id, username AS name
+	FROM customers WHERE email = :email;
+SQL;
 
+$stmt = $dbh->prepare($sql_userSelect);
+$stmt_create = $dbh->prepare($sql_userCreate);
+$stmt_getid = $dbh->prepare($sql_getId);
 
-
-if (isset($_POST['email'])) {
-  $input = $_POST;
-}else{
-  $json = file_get_contents('php://input');
-  $input = json_decode($json,true);      
+if (isset($_POST['email']))
+{
+	$input = $_POST;
+}
+else
+{
+	$json = file_get_contents('php://input');
+	$input = json_decode($json,true);
 }
 
-/* Exeute query */
 $email = $input['email'];
 $password = $input['password'];
+$name = $input['name'];
 
 $stmt->bindParam(':email', $email, PDO::PARAM_STR);
 $stmt->bindParam(':password', $password, PDO::PARAM_STR);
@@ -50,61 +59,68 @@ $result = $stmt->fetchAll();
 
 
 if (!session_start())
-  header("Location: ".$applicationBaseDir."exit.html");
+{
+	header("Location: ".$applicationBaseDir."exit.html");
+	exit();
+}
 
-if (count($result) == 0 && isset($_POST['creditCard'])){        /*User does not exist so we create it.*/
+if (count($result) == 0 && isset($_POST['creditCard']))        /*User does not exist so we create it.*/
+{
+	$stmt_create->bindParam(':username', $name,PDO::PARAM_STR);
+	$stmt_create->bindParam(':email', $email,PDO::PARAM_STR);
+	$stmt_create->bindParam(':creditCard',$input['creditCard'],PDO::PARAM_STR);
+	$stmt_create->bindParam(':expireCard',$input['expireDate'],PDO::PARAM_STR);
+	$stmt_create->bindParam(':password',$password,PDO::PARAM_STR);
 
-  $stmt_create->bindParam(':username',$name,PDO::PARAM_STR);
-  $stmt_create->bindParam(':email',$email,PDO::PARAM_STR);
-  $stmt_create->bindParam(':creditCard',$input['creditCard'],PDO::PARAM_STR);
-  $stmt_create->bindParam(':expireCard',$input['expireDate'],PDO::PARAM_STR);
-  $stmt_create->bindParam(':password',$password,PDO::PARAM_STR);
+	if($stmt_create->execute() === FALSE)
+	{
+		print_r($dbh->errorInfo());
+		return;
+	}
 
-  $stmt_create->execute();
+	$_SESSION['name'] = $name;
+	$_SESSION['email'] = $email;
+	$stmt_getid->bindParam(':email', $email, PDO::PARAM_STR);
 
+	$result_id_name = stmtQuery($stmt_getid);
 
-  $_SESSION['name'] = $name;
-  $_SESSION['email'] = $email;
-  $stmt_getid->bindParam(':email',$email,PDO::PARAM_STR);
+	$_SESSION['id'] = $result_id_name[0]['id'];
+	$_SESSION['name'] = $result_id_name[0]['name'];
 
-  $result_id_name = $stmtQuery($stmt_getid);
+	header("Location: ".$applicationBaseDir."index.php");
+}
+else
+{      /* Now, if we come from a login */
+	if (count($result == 1))
+	{
+		$password = $result[0]['password'];
+		$email = $result[0]['email'];
+		$name = $result[0]['username'];
 
-  $_SESSION['id'] = $result_id_name[0]['id'];
-  $_SESSION['name'] = $result_id_name[0]['name'];
+		if (0 == strcmp($password,$input['password'])){
+			$_SESSION['name'] = $name;
+			$_SESSION['email'] = $email;
+			$stmt_getid->bindParam(':email',$email,PDO::PARAM_STR);
 
+			$result_id_name = stmtQuery($stmt_getid);
 
-  header("Location: ".$applicationBaseDir."index.php");
-}else{      /* Now, if we come from a login */
-  
-  if (count($result == 1)){
-   
-    
-    $password = $result[0]['password'];
-    $email = $result[0]['email'];
-    $name = $result[0]['username'];
-
-    if (0 == strcmp($password,$input['password'])){
-      $_SESSION['name'] = $name;
-      $_SESSION['email'] = $email;
-      $stmt_getid->bindParam(':email',$email,PDO::PARAM_STR);
-
-      $result_id_name = stmtQuery($stmt_getid);
-
-      $_SESSION['id'] = $result_id_name[0]['id'];
+			$_SESSION['id'] = $result_id_name[0]['id'];
 
 
-      $output = array( "name" => $name );
-      echo json_encode($output);
-      http_response_code(200);
-    } else{
-      $_SESSION['name'] = "Mismatch";
-      $_SESSION['email'] = "";
-      $name="";
-      http_response_code(404);
-    }
+			$output = array( "name" => $name );
+			echo json_encode($output);
+			http_response_code(200);
+		} else{
+			$_SESSION['name'] = "Mismatch";
+			$_SESSION['email'] = "";
+			$name="";
+			http_response_code(404);
+		}
 
-  } else{
-    http_response_code(404); 
-  }
+	}
+	else
+	{
+		http_response_code(404);
+	}
 }
 ?>
